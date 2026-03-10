e5508295a8c49b1e1ad470404e183177c584c3e7
yurial3445 <andre.ys@sempreceub.com>
2026-03-10 16:09:03 -0300

    v1.0.0 - o projeto começou agora

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==== Gerenciador de Gastos====");
+        System.out.println("===============================");
+        System.out.println("Aperte (1) para Adicionar gastos");
+        System.out.println("Aperte (2) para  Sair");
+
+        System.out.print("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
+    
+    
+    }
+}
