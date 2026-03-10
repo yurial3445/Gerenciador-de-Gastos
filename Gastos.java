3baa5ce6246a7c481643c11f86cf18a9e8d152d5
yurial3445 <andre.ys@sempreceub.com>
2026-03-10 16:48:07 -0300

    v1.1.0 A classe Gastos ainda esta em andamento e apresenta problemas

--- Gastos.java
+++ Gastos.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Gastos {
+
+        String descricao;
+        double valor;
+        LocalDateTime dataHora;
+
+        //construtor
+        public Gastos(String descricao, double valor) {
+            this.descricao = descricao;
+            this.valor = valor;
+            this.dataHora = LocalDateTime.now();
+        }
+
     
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Gerenciador de Gastos
 Um simples programa que ira calcular seus gastos e salvar.
+
+Projeto:
+
+🔴 Build status: failing
+Coverage: 0%
+Version: 1.1.0
+
+
+
