67ceea0b2dcb98e3b52128e16485677914075343
yurial3445 <andre.ys@sempreceub.com>
2026-03-12 16:35:04 -0300

    v1.2.1 Adicionamento de comentarios, desenvolvimento do main a classe principal e o adicionamento do test.yml

--- Gastos.java
+++ Gastos.java
@@ -1,17 +1,23 @@
-import java.util.ArrayList;
+
 import java.time.LocalDateTime;
-import java.util.Scanner;
+//Importa uma classe da biblioteca padrão do Java para trabalhar com data e hora.
 
 public class Gastos {
-
+//Variaveir para armazenar a descrição do gastos
         String descricao;
         double valor;
         LocalDateTime dataHora;
 
         //construtor
         public Gastos(String descricao, double valor) {
+                        
+            //atributo do objeto atual recebe o valor passado como argumento
             this.descricao = descricao;
+
+            //atributo do objeto atual recebe o valor passado como argumento
             this.valor = valor;
+
+            //atributo do objeto atual recebe a data e hora atual
             this.dataHora = LocalDateTime.now();
         }
 
--- Main.java
+++ Main.java
@@ -1,17 +1,38 @@
+//importar a classe Scanner para ler a entrada do usuário
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Scanner serve para ler dados digitados pelo usuário.
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("==== Gerenciador de Gastos====");
-        System.out.println("===============================");
-        System.out.println("Aperte (1) para Adicionar gastos");
-        System.out.println("Aperte (2) para  Sair");
+        //Variavel de controle para gurada a opção escolhida no menu
+        int opcao = 0;
 
-        System.out.print("Escolha uma opção: ");
-        int opcao = scanner.nextInt();
-    
+        //loop do programa
+        while (opcao !=2){
+            //menu
+            System.out.println("==== Gerenciador de Gastos====");
+            System.out.println("===============================");
+            System.out.println("Aperte (1) para Adicionar gastos");
+            System.out.println("Aperte (2) para  Sair");
+
+            //ler a opção
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            //Consumir quebra de linha pendente
+            scanner.nextLine();
+            
+            //condição caso o usuario escolha a opção 1
+            if(opcao == 1){
+
+                System.out.print("Descrição: ");
+                String descricao = scanner.nextLine();
+            }
+        
+        }
+        
     
     }
 }
