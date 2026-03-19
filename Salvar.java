038e60c93c5abe304ca38708742efb34cd3492d1
yurial3445 <andre.ys@sempreceub.com>
2026-03-19 15:59:53 -0300

    v1.4.1 Finalização dda classe salvar mais ainda deve ser testado

--- Salvar.java
+++ Salvar.java
@@ -1,3 +1,27 @@
-public class Salvar {
-    
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+
+public class Salvar{
+
+    public static void salvar(Gastos gasto) {
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        try {
+
+            FileWriter writer = new FileWriter("gastos.txt", true);
+
+            writer.write(
+                gasto.dataHora.format(formato) + " | " +
+                gasto.descricao + " | R$ " +
+                gasto.valor + "\n"
+            );
+
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar gasto.");
+        }
+    }
 }
