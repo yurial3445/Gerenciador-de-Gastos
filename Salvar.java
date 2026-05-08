67a7d837460fd82e653c0183e2c805db7aaaa896
yurial3445 <andre.ys@sempreceub.com>
2026-05-08 16:16:57 -0300

    salvar melhorado

--- Salvar.java
+++ Salvar.java
@@ -2,34 +2,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
-import Main.Gastos;
+public class Salvar {
 
-public class Salvar{
+    public static void salvar(Gastos gasto) {
 
-    public static void salvar(Gastos gasto ,Leitor leitor) {
-
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        double total = Leitor.mostrarTotal();
+        DateTimeFormatter formato =
+            DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         try {
 
             FileWriter writer = new FileWriter("gastos.txt", true);
 
             writer.write(
-
-                
                 gasto.dataHora.format(formato) + " | " +
                 gasto.descricao + " | R$ " +
-                gasto.valor + "\n"
-                
-                
-                
+                gasto.valor + " | " +
+                gasto.moeda + "\n"
             );
-           FileWriter writer1 = new FileWriter("total.txt");
 
-            writer1.write("Total gasto: R$ " + total);
-
-            writer1.close();
             writer.close();
 
         } catch (IOException e) {
