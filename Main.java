1315f1738eb243bef1826144e6cec309458b1557
yurial3445 <andre.ys@sempreceub.com>
2026-05-08 16:16:16 -0300

    Main consertado

--- Main.java
+++ Main.java
@@ -1,36 +1,26 @@
-//importar a classe Scanner para ler a entrada do usuário
-import Main.CotacaoService;
-import Main.Gastos;
-import Main.Leitor;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        //Scanner serve para ler dados digitados pelo usuário.
         Scanner scanner = new Scanner(System.in);
 
-        //Variavel de controle para gurada a opção escolhida no menu
         int opcao = 0;
 
-        //loop do programa
-        while (opcao !=2){
-            //menu
-            System.out.println("==== Gerenciador de Gastos====");
-            System.out.println("===============================");
-            System.out.println("Aperte (1) para Adicionar gastos");
-            System.out.println("Aperte (2) para  Sair");
-            System.out.println("Aperte (3) para Ver total gasto");
+        while (opcao != 4) {
 
+            System.out.println("==== Gerenciador de Gastos ====");
+            System.out.println("1 - Adicionar gasto");
+            System.out.println("2 - Listar gastos");
+            System.out.println("3 - Ver total gasto");
+            System.out.println("4 - Sair");
 
-            //ler a opção
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
-            //Consumir quebra de linha pendente
             scanner.nextLine();
-            
-            //condição caso o usuario escolha a opção 1
-            if(opcao == 1){
+
+            if (opcao == 1) {
 
                 System.out.print("Descrição: ");
                 String descricao = scanner.nextLine();
@@ -45,36 +35,37 @@ public class Main {
                 String moeda = "BRL";
 
                 if (resposta.equalsIgnoreCase("s")) {
+
                     double cotacao = CotacaoService.buscarDolar();
+
                     valor = valor * cotacao;
                     moeda = "USD";
-                    System.out.println("Convertido para reais: " + valor);
+
+                    System.out.println(
+                        "Convertido para reais: R$ " + valor
+                    );
                 }
 
                 Gastos gasto = new Gastos(descricao, valor, moeda);
-                Salvar.salvar(gasto, null);
+
+                Salvar.salvar(gasto);
 
                 System.out.println("Gasto salvo.");
-                
 
-                if (resposta.equalsIgnoreCase("s")) {
-                        double cotacao = CotacaoService.buscarDolar();
-                        valor = valor * cotacao;
-                        System.out.println("Convertido para reais: " + valor);
-                    }
-            
-            }else if (opcao == 2) {
+            } else if (opcao == 2) {
 
                 Leitor.listar();
 
             } else if (opcao == 3) {
 
                 Leitor.mostrarTotal();
+
+            } else if (opcao == 4) {
+
+                System.out.println("Encerrando sistema...");
             }
-        
         }
-        //Fechar acanner
+
         scanner.close();
-    
     }
 }
